427bac37bf4f5c0f0c434f4dc6e4545ea02273ab
kyb <kybdev@gmail.com>
2022-05-01 13:38:25 +0900

    <:test> 테스트 설정중

---
+++ test/unit/user/user.repository.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '@/user/user.service';
+import { InfraModule } from '@/infra/infra.module';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [InfraModule],
+      providers: [UserService],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('create user', () => {
+    expect(service).toBeDefined();
+    service.createUser({
+      account: 'test123',
+      password: 'test1',
+      name: 'test11',
+    });
+  });
+});
+
